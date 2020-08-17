e47324ea7ef9bc95d0a87e1feccf7f125ec6a786
Natalia Astafieva <demius.md@gmail.com>
2020-08-17 15:40:54 +0300

    initial commit

---
+++ packages/identity/src/index.ts
@@ -0,0 +1,13 @@
+// starter point: https://medium.com/@ramez.aijaz/webcomponents-based-ui-with-typescript-litelement-and-webpack-c0cbbe54d920
+// vaadin starter: https://vaadin.com/learn/tutorials/lit-element/starting-a-lit-element-project
+// declarative events: https://coryrylan.com/blog/using-event-decorators-with-lit-element-and-web-components
+// src with advanced webpack: https://github.com/vaadin-learning-center/lit-element-tutorial-starting-a-lit-element-project
+
+// https://webpack.js.org/guides/production/#setup
+// https://webpack.js.org/configuration/
+
+// TODO: move webpack config to root folder
+// TODO: webpack common, dev, prod
+// TODO: webpack minification
+
+import "./hello-world-elem";
